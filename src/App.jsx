732fa74dc31b012254bb20b72d74f374175e7ce3
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-06-30 21:56:40 +0530

    implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './App.module.css'
 import { Navbar } from './component/Navbar/Navbar'
+import { Hero } from './component/Hero/Hero'
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar/>
+      <Hero />
     </div>
   )
 }
---
+++ src/component/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from "./Hero.module.css";
+import {getImageUrl} from '../../utils';
+
+export const Hero = () => {
+
+    const heroImgUrl = getImageUrl("hero/heroImage.png");
+
+  return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Nadeesha</h1>
+            <p className={styles.description}>I am a java developer with no experiences. undergraduate University of Kelaniya</p>
+            <a className={styles.contactBtn} href='mailto:piyuminadeesha.22@mail.com'></a>
+        </div>
+        <img className={styles.heroImg} src={heroImgUrl}/>
+        <div className={styles.topBlur}/>
+        <div className={styles.bottomBlur}/>
+    </section>
+  )
+}
