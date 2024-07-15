3154cf43d66238b99f89f13b7f8376d40719e61a
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-07-15 18:50:45 +0530

    Finish Experience Section

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ export const Navbar = () => {
       <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}` }
       onClick={()=> setMenuOpen(false)}>
         <li><a href='#about'>About</a></li>
-        <li><a href='#experiences'>Experiences</a></li>
+        <li><a href='#experience'>Experiences</a></li>
         <li><a href='#projects'>Projects</a></li>
         <li><a href='#contact'>Contact</a></li>
       </ul>
